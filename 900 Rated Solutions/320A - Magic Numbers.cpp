efdba509b06e4992f0e2eed522f555b3a20a99ed
Swaraj Kumar <95646698+swaraj29@users.noreply.github.com>
2024-06-25 15:04:08 +0530

    Update 320A - Magic Numbers.cpp

--- 900 Rated Solutions/320A - Magic Numbers.cpp
+++ 900 Rated Solutions/320A - Magic Numbers.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <regex>
+using namespace std;
+
+bool magic(string &n){
+    regex p("^(1|14|144)*$");
+    return regex_match(n, p);
+}
+
+int main()
+{
+    string n;
+    cin >> n;
+    
+    if (magic(n)){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
 
